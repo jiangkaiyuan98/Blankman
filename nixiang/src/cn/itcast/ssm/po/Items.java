d9e9b0d2a6941df1910ee90bf27dac4e4b136c46
jiangkaiyuan98 <49426666+jiangkaiyuan98@users.noreply.github.com>
2019-05-12 10:07:43 +0800

    Add files via upload

---
+++ nixiang/src/cn/itcast/ssm/po/Items.java
@@ -0,0 +1,55 @@
+package cn.itcast.ssm.po;
+
+import java.util.Date;
+
+public class Items {
+    private Integer id;
+
+    private String name;
+
+    private Integer price;
+
+    private Date createtime;
+
+    private String detail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail == null ? null : detail.trim();
+    }
+}
